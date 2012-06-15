0ab404db9f27cde0bf4e69ab16eb2b257af7a25b
Trevor Norris <trev.norris@gmail.com>
2012-06-15 15:12:32 -0700

    really trying to get over using excessive whitespace

--- cbuffer.js
+++ cbuffer.js
@@ -1,13 +1,13 @@
-(function( global ) {
+(function(global) {
 
 function CBuffer() {
 	var i = 0;
 	// handle cases where "new" keyword wasn't used
-	if (!( this instanceof CBuffer )) {
-		if ( arguments.length > 1 || typeof arguments[0] !== 'number' ) {
-			return CBuffer.apply( new CBuffer(), arguments );
+	if (!(this instanceof CBuffer)) {
+		if (arguments.length > 1 || typeof arguments[0] !== 'number') {
+			return CBuffer.apply(new CBuffer(), arguments);
 		} else {
-			return new CBuffer( arguments[0] );
+			return new CBuffer(arguments[0]);
 		}
 	}
 	// this is the same in either scenario
@@ -15,17 +15,17 @@ function CBuffer() {
 	// set to callback fn if data is about to be overwritten
 	this.overflow = false;
 	// build CBuffer based on passed arguments
-	if ( arguments.length > 1 || typeof arguments[0] !== 'number' ) {
-		this.data = new Array( arguments.length );
-		this.end = ( this.length = arguments.length ) - 1;
-		this.push.apply( this, arguments );
+	if (arguments.length > 1 || typeof arguments[0] !== 'number') {
+		this.data = new Array(arguments.length);
+		this.end = (this.length = arguments.length) - 1;
+		this.push.apply(this, arguments);
 	} else {
-		this.data = new Array( arguments[0] );
+		this.data = new Array(arguments[0]);
 		// force preallocation of memory to each array slot, for quicker operation on buffer
-		for ( ; i < arguments[0]; i++ ) {
+		for (; i < arguments[0]; i++) {
 			this.data[i] = undefined;
 		}
-		this.end = ( this.length = arguments[0] ) - 1;
+		this.end = (this.length = arguments[0]) - 1;
 	}
 	return this;
 }
@@ -38,37 +38,37 @@ CBuffer.prototype = {
 	// pop last item
 	pop : function() {
 		var item;
-		if ( this.size === 0 ) return;
-		item = this.data[ this.end ];
-		delete this.data[( this.size + this.start - 1 ) % this.length ];
+		if (this.size === 0) return;
+		item = this.data[this.end];
+		delete this.data[(this.size + this.start - 1) % this.length];
 		this.size--;
-		this.end = ( this.end - 1 + this.length ) % this.length;
+		this.end = (this.end - 1 + this.length) % this.length;
 		return item;
 	},
 	// push item to the end
 	push : function() {
 		var i = 0;
 		// check if overflow is set, and if data is about to be overwritten
-		if ( this.overflow && this.size + arguments.length > this.length ) {
+		if (this.overflow && this.size + arguments.length > this.length) {
 			// call overflow function and send data that's about to be overwritten
-			for ( ; i < this.size + arguments.length - this.length; i++ ) {
-				this.overflow( this.data[( this.end + i + 1 ) % this.length ], this );
+			for (; i < this.size + arguments.length - this.length; i++) {
+				this.overflow(this.data[(this.end + i + 1) % this.length], this);
 			}
 		}
 		// push items to the end, wrapping and erasing existing items
 		// using arguments variable directly to reduce gc footprint
-		for ( i = 0; i < arguments.length; i++ ) {
-			this.data[( this.end + i + 1 ) % this.length ] = arguments[i];
+		for (i = 0; i < arguments.length; i++) {
+			this.data[(this.end + i + 1) % this.length] = arguments[i];
 		}
 		// recalculate size
-		if ( this.size < this.length ) {
-			if ( this.size + i > this.length ) this.size = this.length;
+		if (this.size < this.length) {
+			if (this.size + i > this.length) this.size = this.length;
 			else this.size += i;
 		}
 		// recalculate end
-		this.end = ( this.end + i ) % this.length;
+		this.end = (this.end + i) % this.length;
 		// recalculate start
-		this.start = ( this.length + this.end - this.size + 1 ) % this.length;
+		this.start = (this.length + this.end - this.size + 1) % this.length;
 		// return number current number of items in CBuffer
 		return this.size;
 	},
@@ -76,26 +76,26 @@ CBuffer.prototype = {
 	reverse : function() {
 		var i = 0,
 			tmp;
-		for ( ; i < ~~( this.size / 2 ); i++ ) {
-			tmp = this.data[( this.start + i ) % this.length ];
-			this.data[( this.start + i ) % this.length ] = this.data[( this.start + ( this.size - i - 1 )) % this.length ];
-			this.data[( this.start + ( this.size - i - 1 )) % this.length ] = tmp;
+		for (; i < ~~(this.size / 2); i++) {
+			tmp = this.data[(this.start + i) % this.length];
+			this.data[(this.start + i) % this.length] = this.data[(this.start + (this.size - i - 1)) % this.length];
+			this.data[(this.start + (this.size - i - 1)) % this.length] = tmp;
 		}
 		return this;
 	},
 	// rotate buffer to the left by cntr, or by 1
-	rotateLeft : function( cntr ) {
-		if ( !cntr ) cntr = 1;
-		while ( --cntr >= 0 ) {
-			this.push( this.shift());
+	rotateLeft : function(cntr) {
+		if (!cntr) cntr = 1;
+		while (--cntr >= 0) {
+			this.push(this.shift());
 		}
 		return this;
 	},
 	// rotate buffer to the right by cntr, or by 1
-	rotateRight : function( cntr ) {
-		if ( !cntr ) cntr = 1;
-		while ( --cntr >= 0 ) {
-			this.unshift( this.pop());
+	rotateRight : function(cntr) {
+		if (!cntr) cntr = 1;
+		while (--cntr >= 0) {
+			this.unshift(this.pop());
 		}
 		return this;
 	},
@@ -103,20 +103,20 @@ CBuffer.prototype = {
 	shift : function() {
 		var item;
 		// check if there are any items in CBuff
-		if ( this.size === 0 ) return;
+		if (this.size === 0) return;
 		// store first item for return
-		item = this.data[ this.start ];
+		item = this.data[this.start];
 		// delete first item from memory
-		delete this.data[ this.start ];
+		delete this.data[this.start];
 		// recalculate start of CBuff
-		this.start = ( this.start + 1 ) % this.length;
+		this.start = (this.start + 1) % this.length;
 		// decrement size
 		this.size--;
 		return item;
 	},
 	// sort items
-	sort : function( fn ) {
-		if ( fn ) this.data.sort( fn );
+	sort : function(fn) {
+		if (fn) this.data.sort(fn);
 		else this.data.sort();
 		this.start = 0;
 		this.end = this.size - 1;
@@ -126,88 +126,88 @@ CBuffer.prototype = {
 	unshift : function() {
 		var i = 0;
 		// check if overflow is set, and if data is about to be overwritten
-		if ( this.overflow && this.size + arguments.length > this.length ) {
+		if (this.overflow && this.size + arguments.length > this.length) {
 			// call overflow function and send data that's about to be overwritten
-			for ( ; i < this.size + arguments.length - this.length; i++ ) {
-				this.overflow( this.data[ this.end - ( i % this.length )], this );
+			for (; i < this.size + arguments.length - this.length; i++) {
+				this.overflow(this.data[this.end - (i % this.length)], this);
 			}
 		}
-		for ( i = 0; i < arguments.length; i++ ) {
-			this.data[( this.length + this.start - ( i % this.length ) - 1 ) % this.length ] = arguments[i];
+		for (i = 0; i < arguments.length; i++) {
+			this.data[(this.length + this.start - (i % this.length) - 1) % this.length] = arguments[i];
 		}
-		if ( this.length - this.size - i < 0 ) {
+		if (this.length - this.size - i < 0) {
 			this.end += this.length - this.size - i;
-			if ( this.end < 0 ) this.end = this.length + ( this.end % this.length );
+			if (this.end < 0) this.end = this.length + (this.end % this.length);
 		}
-		if ( this.size < this.length ) {
-			if ( this.size + i > this.length ) this.size = this.length;
+		if (this.size < this.length) {
+			if (this.size + i > this.length) this.size = this.length;
 			else this.size += i;
 		}
 		this.start -= arguments.length;
-		if ( this.start < 0 ) this.start = this.length + ( this.start % this.length );
+		if (this.start < 0) this.start = this.length + (this.start % this.length);
 		return this.size;
 	},
 
 	/* accessor methods */
 	// return index of first matched element
-	indexOf : function( arg, idx ) {
-		if ( !idx ) idx = 0;
-		for ( ; idx < this.size; idx++ ) {
-			if ( this.data[( this.start + idx ) % this.length ] === arg ) return idx;
+	indexOf : function(arg, idx) {
+		if (!idx) idx = 0;
+		for (; idx < this.size; idx++) {
+			if (this.data[(this.start + idx) % this.length] === arg) return idx;
 		}
 		return -1;
 	},
 	// return last index of the first match
-	lastIndexOf : function( arg, idx ) {
-		if ( !idx ) idx = this.size - 1;
-		for ( ; idx >= 0; idx-- ) {
-			if ( this.data[( this.start + idx ) % this.length ] === arg ) return idx;
+	lastIndexOf : function(arg, idx) {
+		if (!idx) idx = this.size - 1;
+		for (; idx >= 0; idx--) {
+			if (this.data[(this.start + idx) % this.length] === arg) return idx;
 		}
 		return -1;
 	},
 
 	/* iteration methods */
 	// check every item in the array against a test
-	every : function( callback, context ) {
+	every : function(callback, context) {
 		var i = 0;
-		if ( context ) {
-			for ( ; i < this.size; i++ ) {
-				if ( !callback.call( context, this.data[( this.start + i ) % this.length ], i, this ))
+		if (context) {
+			for (; i < this.size; i++) {
+				if (!callback.call(context, this.data[(this.start + i) % this.length], i, this))
 					return false;
 			}
 		} else {
-			for ( ; i < this.size; i++ ) {
-				if ( !callback( this.data[( this.start + i ) % this.length ], i, this ))
+			for (; i < this.size; i++) {
+				if (!callback(this.data[(this.start + i) % this.length], i, this))
 					return false;
 			}
 		}
 		return true;
 	},
 	// loop through each item in buffer
-	forEach : function( callback, context ) {
+	forEach : function(callback, context) {
 		var i = 0;
 		// check if context was passed
-		if ( context ) {
-			for ( ; i < this.size; i++ ) {
-				callback.call( context, this.data[( this.start + i ) % this.length ], i, this );
+		if (context) {
+			for (; i < this.size; i++) {
+				callback.call(context, this.data[(this.start + i) % this.length], i, this);
 			}
 		} else {
-			for ( ; i < this.size; i++ ) {
-				callback( this.data[( this.start + i ) % this.length ], i, this );
+			for (; i < this.size; i++) {
+				callback(this.data[(this.start + i) % this.length], i, this);
 			}
 		}
 	},
 	// check items agains test until one returns true
-	some : function( callback, context ) {
+	some : function(callback, context) {
 		var i = 0;
-		if ( context ) {
-			for ( ; i < this.size; i++ ) {
-				if ( callback.call( context, this.data[( this.start + i ) % this.length ], i, this ))
+		if (context) {
+			for (; i < this.size; i++) {
+				if (callback.call(context, this.data[(this.start + i) % this.length], i, this))
 					return true;
 			}
 		} else {
-			for ( ; i < this.size; i++ ) {
-				if ( callback( this.data[( this.start + i ) % this.length ], i, this ))
+			for (; i < this.size; i++) {
+				if (callback(this.data[(this.start + i) % this.length], i, this))
 					return true;
 			}
 		}
@@ -217,12 +217,12 @@ CBuffer.prototype = {
 	/* utility methods */
 	// empty out all values in cbuffer
 	// default is to delete values, but if pass arg then will set all values to arg
-	empty : function( arg ) {
+	empty : function(arg) {
 		var i = 0;
-		if ( arg == null ) {
-			while( delete this.data[i], ++i < this.size );
+		if (arg == null) {
+			while(delete this.data[i], ++i < this.size);
 		} else {
-			while( this.data[i] = arg, ++i < this.size );
+			while(this.data[i] = arg, ++i < this.size);
 		}
 		this.size = this.start = 0;
 		this.end = this.length - 1;
@@ -230,32 +230,32 @@ CBuffer.prototype = {
 	},
 	// return first item in buffer
 	first : function() {
-		return this.data[ this.start ];
+		return this.data[this.start];
 	},
 	// return last item in buffer
 	last : function() {
-		return this.data[ this.end ];
+		return this.data[this.end];
 	},
 	// return specific index in buffer
-	get : function( arg ) {
-		return this.data[( this.start + arg ) % this.length ];
+	get : function(arg) {
+		return this.data[(this.start + arg) % this.length];
 	},
 	// set value at specified index
-	set : function( idx, arg ) {
-		return this.data[( this.start + idx ) % this.length ] = arg;
+	set : function(idx, arg) {
+		return this.data[(this.start + idx) % this.length] = arg;
 	},
 	// return clean array of values
 	toArray : function() {
-		var narr = new Array( this.size ),
+		var narr = new Array(this.size),
 			i = 0;
-		for ( ; i < this.size; i++ ) {
-			narr[i] = this.data[( this.start + i ) % this.length ];
+		for (; i < this.size; i++) {
+			narr[i] = this.data[(this.start + i) % this.length];
 		}
 		return narr;
 	}
 };
 
-if ( typeof module === 'object' && module.exports ) module.exports = CBuffer;
+if (typeof module === 'object' && module.exports) module.exports = CBuffer;
 else global.CBuffer = CBuffer;
 
-}( this ));
+}(this));
